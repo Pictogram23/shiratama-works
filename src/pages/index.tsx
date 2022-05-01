c54b4131e725e552b4af2ed774912a15890bd570
Pictogram23 <shanchengyinger@gmail.com>
2022-05-01 20:03:59 +0900

    開発環境の構築完了まで

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>クラーク Ga専攻 作品集</title>
+      </Head>
+    </>
+  )
+}
+
+export default HomePage
